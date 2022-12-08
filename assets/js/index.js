e139c0868a41684db9015a451cadeb847abf9e5c
donsonic9 <donsonic9@gmail.com>
2022-12-08 15:32:37 -0300

    update-8-12-22-better-performance

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,16 +11,16 @@ let counter = 0;
 colorButton.addEventListener("click", ()=>{
 
   if (counter == 0) {
-    cssChange.setAttribute("href", "assets/css/styles2.css");
+    cssChange.setAttribute("href", "assets/css/custom-2.css");
     counter = 1;
   } else if (counter == 1) {
-    cssChange.setAttribute("href", "assets/css/styles3.css");
+    cssChange.setAttribute("href", "assets/css/custom-3.css");
     counter = 2;
   } else if (counter == 2) {
-    cssChange.setAttribute("href", "assets/css/styles4.css");
+    cssChange.setAttribute("href", "assets/css/custom-4.css");
     counter = 3;
   } else if (counter == 3) {
-    cssChange.setAttribute("href", "assets/css/styles.css");
+    cssChange.setAttribute("href", "assets/css/custom-1.css");
     counter = 0;
   }
 
